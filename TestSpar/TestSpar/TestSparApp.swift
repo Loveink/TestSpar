b8863ab0318b61b1354c3882e4a5b91b1031d251
Loveink <106765014+Loveink@users.noreply.github.com>
2024-02-01 17:55:52 +0300

    initial commit

---
+++ TestSpar/TestSpar/TestSparApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSparApp.swift
+//  TestSpar
+//
+//  Created by Александра Савчук on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
